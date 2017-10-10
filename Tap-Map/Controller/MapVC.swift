92ab63bd03d31a1a7d3c12824fb3c12e953c48dd
Nomad <nomad@Nomads-MBP.Dlink>
2017-10-10 10:59:55 +0300

    Creation of progect

---
+++ Tap-Map/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  Tap-Map
+//
+//  Created by Nomad on 10/10/17.
+//  Copyright © 2017 ios.Nomad. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
